c498847a375018ef97a020f84100f52f70df4506
dev.tarasiuk <sasha50475@gmail.com>
2020-04-25 12:49:46 +0300

    допилил функции остановки и возобновления смены фактов

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -12,44 +12,117 @@ class HomePage extends React.Component {
 
         this.state = {
             fact: null,
-            duration: 5750,
-            factTimer: 0,
+            factProgress: 0,
             step: 250,
-            endAfter: 0
+            duration: 5750,
+            endAfter: 0,
+            statusTimer: 0, // 1 = satrt, 2 = paused, 3 = resume, 0 = null
+            durationEndAfter: 5750
         }
     }
 
     componentDidMount() {
-        this.changeFacts();
+        this.changeFunc();
 
-        this.interval = setInterval(this.changeFacts, this.state.duration);
+        this.timer = this.intervalTimer(this.changeFunc, this.state.duration);
+
+        document.querySelector('.facts').addEventListener('mouseover', () => {
+            this.timerPause();
+        });
+        document.querySelector('.facts').addEventListener('mouseleave', () => {
+            this.timerResume();
+        });
+        document.querySelector('.facts').addEventListener('touchstart', () => {
+            this.timerPause();
+        });
+        document.querySelector('.facts').addEventListener('touchend', () => {
+            this.timerResume();
+        });
+
     }
 
     componentWillUnmount() {
         clearInterval(this.interval); // Clear the interval right before component unmount
     }
 
-    changeFacts = () => {
-        clearInterval(this.interval2); // Clear the interval right before component unmount
+
+
+
+
+
+    changeProgress = () => {
+        this.interval2 = setInterval(() => {
+            this.setState({
+                factProgress: this.state.endAfter / 5000 * 100,
+                endAfter: this.state.endAfter + 250
+            })
+        }, 250)
+    }
+
+    changeDurationEndAfter = () => {
+        this.setState({ durationEndAfter: this.state.durationEndAfter - 250 })
+    }
+
+
+
+    changeFunc = () => {
+        window.clearInterval(this.intervalDuration);
+        clearInterval(this.interval2); 
 
         this.setState({
             fact: factsData[Math.floor(Math.random() * factsData.length)].text,
-            factTimer: 0,
+            durationEndAfter: 5750,
+            factProgress: 0,
             step: 250,
             endAfter: 0
         })
 
         this.changeProgress()
+
+        this.intervalDuration = window.setInterval(this.changeDurationEndAfter, 250)
     }
 
-    changeProgress = () => {
-        this.interval2 = setInterval(() => {
-            this.setState({
-                factTimer: this.state.endAfter / 5000 * 100,
-                endAfter: this.state.endAfter + 250
-            })
-        }, 250)
+
+    
+    // ####################### ACTIONS #######################
+    intervalTimer = (callback, interval) => {
+        this.timerId = window.setInterval(callback, interval);
+        this.setState({ statusTimer: 1 })
     }
+
+    timerPause = () => {
+        window.clearTimeout(this.timeOut);
+        clearInterval(this.interval2); 
+        clearInterval(this.intervalDuration); 
+        if (this.state.statusTimer !== 1) return;
+        
+        window.clearInterval(this.timerId);
+        this.setState({ statusTimer: 2 })
+        console.log(this.state.durationEndAfter);
+        
+    }
+
+    timerResume = () => {
+        if (this.state.statusTimer !== 2) return;
+        
+        this.changeProgress()
+        this.intervalDuration = window.setInterval(this.changeDurationEndAfter, 250)
+        this.setState({ statusTimer: 1 })
+        
+        console.log(this.state.durationEndAfter);
+        this.timeOut = window.setTimeout(this.timerTimeoutCallback, this.state.durationEndAfter);
+    }
+
+    timerTimeoutCallback = () => {
+        // if (this.state.statusTimer !== 1) return;
+
+        this.changeFunc();
+
+        this.timerId = window.setInterval(this.changeFunc, this.state.duration);
+        this.setState({ statusTimer: 1 })
+    };
+    // ####################### END ACTIONS #######################
+
 
     render() {
         return (
@@ -59,7 +132,7 @@ class HomePage extends React.Component {
                 </Container>
                 <FactsContainer>
                     {this.state.fact}
-                    <ProgressBar now={this.state.factTimer} /> 
+                    <ProgressBar now={this.state.factProgress} /> 
                 </FactsContainer>
                 <Container>
                    <RecSlider itemData={recData[1]} />
