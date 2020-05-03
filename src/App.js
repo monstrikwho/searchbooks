264873c3cb1c87828d7d80f14182486e2d276c30
dev.tarasiuk <sasha50475@gmail.com>
2020-05-03 22:21:46 +0300

    redirect add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "./sass/app.sass";
 
 import Navs from "./components/navs";
@@ -118,12 +118,9 @@ export default class App extends React.Component {
                 />
               )}
             />
+            <Redirect to={process.env.PUBLIC_URL + "/"} />
           </Switch>
         </div>
-
-        {/* <Route path='/page' render={(props) => (
-          <Page {...props} data={extraProps}/>
-        )}/> */}
       </Router>
     );
   }
